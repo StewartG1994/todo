da428815fa932a775424eeedd34e7965d77019f1
Stewart Gliniecki <stewgliniecki@gmail.com>
2021-09-29 21:01:45 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './styles.css'
 
 console.log('testTwo')
+
+const core  = (() =>{
+
+    const taskFactory = (task, description, duedate,priority, notes) =>{
+        return {task, description,duedate, priority,notes}
+
+    }
+
+    return taskFactory
+})()
+
+
+
